1ff8d0c4053607692c6d6d0a605f351a2b95d85e
unknown <patrickkeita80@yahoo.com>
2023-01-07 03:31:36 -0800

    Update index.js file

--- src/index.js
+++ src/index.js
@@ -1,65 +1,8 @@
-import Leaderboard from './modules/Leaderboard.js';
-import Display from './modules/Display.js';
+import { fetchData, sendData } from './modules/Leaderboard.js';
 
-const name = document.getElementById('name');
-const score = document.getElementById('score');
-const submit = document.querySelector('.submit');
-const refresh = document.querySelector('.refresh');
+const refresh = document.getElementById('refresh');
+const form = document.getElementById('submit');
 
-const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Oawl0gMZlyz9hcMNVFUQ';
-const api = new Leaderboard(url);
-
-// On Page Load
-const requestUrl = `${url}/scores/`;
-api.getData(requestUrl);
-
-setTimeout(() => {
-  document.addEventListener('DOMContentLoaded', () => {
-    api.getData(requestUrl);
-  });
-}, 1000);
-
-const display = new Display();
-
-// Submit Button Event
-submit.addEventListener('click', (e) => {
-  e.preventDefault();
-  if (e.key === 'Enter') {
-    submit.click();
-  }
-  if (!name.value.trim()) {
-    name.setCustomValidity('Please check input name field.');
-    name.reportValidity();
-  } else if (!score.value) {
-    score.setCustomValidity('Please check input score field.');
-    score.reportValidity();
-  } else if (name.value.trim() && score.value) {
-    api.addData(requestUrl, name.value.trim(), score.value);
-    submit.setCustomValidity('"Leaderboard score created correctly.');
-    submit.reportValidity();
-    name.value = '';
-    score.value = '';
-  }
-  setTimeout(() => {
-    api.getData(requestUrl);
-    const arr = JSON.parse(localStorage.getItem('data'));
-    display.createList(arr);
-  }, 1000);
-});
-
-// Refresh Button Event
-refresh.addEventListener('click', () => {
-  api.getData(requestUrl);
-  if (localStorage.getItem('data')) {
-    const arr = JSON.parse(localStorage.getItem('data'));
-    display.createList(arr);
-  }
-});
-
-// On Page Load
-window.onload = () => {
-  if (localStorage.getItem('data')) {
-    const arr = JSON.parse(localStorage.getItem('data'));
-    display.createList(arr);
-  }
-};
+refresh.addEventListener('click', fetchData);
+form.addEventListener('submit', sendData);
+window.addEventListener('load', fetchData);
